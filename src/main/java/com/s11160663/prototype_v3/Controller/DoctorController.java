d2382b4efaf41e2a4467452852240b21591e0467
DMH24Doug <114621949+DMH24Doug@users.noreply.github.com>
2024-09-02 13:38:01 +1200

    Douglas (updated)
    New updates for the prototype

--- src/main/java/com/s11160663/prototype_v3/Controller/DoctorController.java
+++ src/main/java/com/s11160663/prototype_v3/Controller/DoctorController.java
@@ -1,13 +1,30 @@
 package com.s11160663.prototype_v3.Controller;
 
+import com.s11160663.prototype_v3.DTO.DoctorDTO;
+import com.s11160663.prototype_v3.DTO.PatientDTO;
+import com.s11160663.prototype_v3.Service.DoctorService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class DoctorController {
 
-    @GetMapping("/doctor")
-    public String doctor() {
+    private DoctorService doctorService;
+
+    @Autowired
+    public DoctorController(DoctorService doctorService) {
+        this.doctorService = doctorService;
+    }
+
+    @GetMapping("/doctor/doctor_home/{Id}")
+    public String patient_save(@PathVariable("Id") Long id, Model model) {
+        DoctorDTO doctorDTO = doctorService.findDoctorById(id);
+        model.addAttribute("doctor", doctorDTO);
+
+        //return view for patient home
         return "doctor_home";
     }
 
